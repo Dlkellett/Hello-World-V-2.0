2c1d073d1fe3b05010018c6a63498601229be78b
Dylan Kellett <DKellett1@my.gcu.edu>
2020-09-19 20:47:01 +0000

    finalizing my first creation

--- Hello World V 2.0/Form1.cs
+++ Hello World V 2.0/Form1.cs
@@ -16,22 +16,22 @@ namespace Hello_World_V_2._0
         {
             InitializeComponent();
         }
-        //Editing text for second button 
+        //Pushing text to second button 
         private void button1_Click(object sender, EventArgs e)
         {
             helloButton2.Text = "And then click me!!!";
         }
-        //Editing text for thrid button 2
+        //Pushing text to thrid button 
         private void button2_Click(object sender, EventArgs e)
         {
             helloButton3.Text = "Then finally click me!!!";
         }
-        
+        //Pushing text to text box
         private void button3_Click(object sender, EventArgs e)
         {
             helloWorldText.Text = "Hello this sweet world of ours!!!";
         }
-
+        //Pushing text to first button
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
         {
             helloButton1.Text = "Now click me!!!";
